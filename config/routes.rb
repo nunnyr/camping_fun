311eec07d3209d4d80648d8e181b3af2d7bd69dc
BellaEvalle <bellaevalle@yahoo.com>
2020-08-27 17:39:43 -0400

    working on activities

--- config/routes.rb
+++ config/routes.rb
@@ -2,20 +2,8 @@ Rails.application.routes.draw do
   get '/campers', to: "campers#index", as: "campers"
   get '/campers/:id', to: "campers#show", as: "camper"
 
-
-
-
-
-
-
-
-
-
-
-
-
   get '/activities', to: "activities#index", as: "activities"
-
+  get '/activities/:id', to: "activities#show", as: "activity"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
 
